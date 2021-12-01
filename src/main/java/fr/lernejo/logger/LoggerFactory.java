20e4fc541578ea86bfb5569e43a8f6804708f47c
Jeremy Vissault <vissault@et.esiea.fr>
2021-12-01 21:50:38 +0100

    Partie 6

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,9 +1,11 @@
 package fr.lernejo.logger;
 
 
+import fr.lernejo.guessgame.CompositeLogger;
+
 public class LoggerFactory{
     public static Logger getLogger(String Name){
-        return new ContextualLogger(new FileLogger("FileLog.log"));
+        return new ContextualLogger(new CompositeLogger(new ConsoleLogger(),new FileLogger("Filelog.log")));
     }
 }
 
